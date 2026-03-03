94906e66efb081df87d93229ccb09ef3664bfa1c
Holliday <mackeybenjamin1@gmail.com>
2026-03-03 17:48:17 -0400

    backup commit

---
+++ GameProgrammingii_MonogameRPG_BenjaminMackey/Scripts/Adjustable/EnemyManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProgrammingii_MonogameRPG_BenjaminMackey.Scripts.Adjustable
+{
+
+    //enemy type ideas
+
+    //the move if you're close to them and then blow up
+
+    //the move horizontally straight across the track
+
+    //driving down the track normal, and just have one movment type for now but like make a few more later, maybe add key charachters? defo in future lol
+    internal class EnemyManager
+    {
+    }
+}
---
+++ GameProgrammingii_MonogameRPG_BenjaminMackey/Scripts/Adjustable/PowerupManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProgrammingii_MonogameRPG_BenjaminMackey.Scripts.Adjustable
+{
+    //general pickup class
+
+    //extend into boost pickup
+    //mario star type thing
+    //ghost ability
+
+    //eventually add things to attack the enemies
+    internal class PowerupManager
+    {
+    }
+}
